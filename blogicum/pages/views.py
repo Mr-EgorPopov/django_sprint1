8c4f161ae383fba9e1357a118298b73babdf816d
Egor Popov <popovyegor1999@gmail.com>
2025-01-12 17:44:59 +0300

    Прошу проверить выполнение проектного задания

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 
 
 def about(request) -> HttpResponse:
-    """Отрисовка страницы 'О проекте' """
+    """Отрисовка страницы 'О проекте'"""
     template = 'pages/about.html'
     return render(request, template)
 
 
 def rules(request) -> HttpResponse:
-    """Отрисовка страницы 'Наши Правила' """
+    """Отрисовка страницы 'Наши Правила'"""
     template = 'pages/rules.html'
     return render(request, template)
